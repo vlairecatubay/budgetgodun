163661d20d9e87e35e7183f8d55357b6e509b2f6
Valerie Catubay <vlaire.catubay@gmail.com>
2026-07-10 04:35:17 +0800

    Updated

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "budgetgodun-cache-v1";
+const CACHE_NAME = "budgetgodun-cache-v2";
 const ASSETS = ["./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", function (event) {
