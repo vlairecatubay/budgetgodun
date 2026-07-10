eae1c0c92b8e7d9f734f188102a2e7360da188cb
valerie-dss <valerie.catubay@dss.asia>
2026-07-11 02:05:45 +0800

    Latest update
    Latest update

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "budgetgodun-cache-v2";
+const CACHE_NAME = "budgetgodun-cache-v5";
 const ASSETS = ["./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", function (event) {
